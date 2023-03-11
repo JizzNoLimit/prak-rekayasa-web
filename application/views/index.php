b8d301d9d34bc80c2e4c0a317cad77d78ddb59ed
BubbleJizun <muhammadajiz975@gmail.com>
2023-03-12 00:34:24 +0700

    mengubah root url, welcome_message -> index

---
+++ application/views/index.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<?php include 'template/header.php'; ?>
+
+    <h1>Hello world!!!</h1>
+    <p>Ini adalah halaman home</p>
+
+<?php include 'template/footer.php'; ?>
---
+++ application/views/template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="shortcut icon" href="./assets/prak-rekayasa-web.ico" type="image/x-icon">
+</head>
+
+<body>
