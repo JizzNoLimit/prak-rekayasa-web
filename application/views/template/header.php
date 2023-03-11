5621895c67c54e1ffbcbea2b21972a705c512af9
BubbleJizun <muhammadajiz975@gmail.com>
2023-03-12 00:58:08 +0700

    mengubah configurasi link css

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="shortcut icon" href="./assets/prak-rekayasa-web.ico" type="image/x-icon">
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
+    <link rel="shortcut icon" href="<?= base_url(); ?>/assets/prak-rekayasa-web.ico" type="image/x-icon">
 </head>
 
 <body>
