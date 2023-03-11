1c8644312a96ed94c93a8e0abc33fbf393a9828d
BubbleJizun <muhammadajiz975@gmail.com>
2023-03-12 01:44:07 +0700

    configurasi bootstrap 5

--- application/views/index.php
+++ application/views/index.php
@@ -5,6 +5,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <?php include 'template/header.php'; ?>
 
     <h1>Hello world!!!</h1>
-    <p>Ini adalah halaman home</p>
+    <p class="title-mono">Ini adalah halaman home</p>
 
 <?php include 'template/footer.php'; ?>
